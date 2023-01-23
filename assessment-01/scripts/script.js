fc19ecbc89f7e3c55d25702a6cfaa962af6cb6cb
Sophie <89017031+SchoolRulez@users.noreply.github.com>
2023-01-23 14:11:53 -0600

    Add files via upload

---
+++ assessment-01/scripts/script.js
@@ -0,0 +1,14 @@
+function externalFunction() {
+    document.getElementById("para").innerHTML="The button used an external script to change me!";
+    }
+
+function imageSize() {
+    var myImg=document.getElementById("bird");
+    myImg.style.width="300px";
+    myImg.style.height="300px";
+}
+
+/*
+I did use another resource for changing the image size that I found helpful:
+https://www.tutorialrepublic.com/faq/how-to-increase-and-decrease-image-size-using-javascript.php#:~:text=Answer%3A%20Use%20the%20JavaScript%20width,in%20and%20zoom%2Dout%20feature.
+*/
